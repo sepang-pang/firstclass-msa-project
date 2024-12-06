d9c8b4428017c39f1674efcb3305ee92728d5634
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-06 21:30:06 +0900

    [ INIT ] eureka-server init commit

--- com.first-class.msa.server/src/main/java/com/first_class/msa/server/ServerApplication.java
+++ com.first-class.msa.server/src/main/java/com/first_class/msa/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.first_class.msa.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServerApplication {
 
 	public static void main(String[] args) {
