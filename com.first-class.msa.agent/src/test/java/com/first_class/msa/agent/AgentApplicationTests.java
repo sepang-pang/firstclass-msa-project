a5e4eb131454a9fb1ae7c2986bb83296277b6254
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-06 22:05:09 +0900

    [ TEST ] 간단한 서비스 기능 테스트 ( #1 )

--- com.first-class.msa.agent/src/test/java/com/first_class/msa/agent/AgentApplicationTests.java
+++ com.first-class.msa.agent/src/test/java/com/first_class/msa/agent/AgentApplicationTests.java
@@ -1,13 +1,20 @@
 package com.first_class.msa.agent;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AgentApplicationTests {
 
+
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testServiceFunctionality() {
+		System.out.println("빌드 자동화 테스트 성공");
+	}
 }
