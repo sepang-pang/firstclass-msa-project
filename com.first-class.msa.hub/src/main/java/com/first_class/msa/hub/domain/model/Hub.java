a82795f4eeec6f3651edaada748b9e28d59b01bc
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-09 22:46:22 +0900

    [ FEAT ] 허브 엔티티 생성 ( #6 )
    - 허브 ID, 이름, 주소, 위도 및 경도 등의 필드 추가
    
    - 생성 및 수정 시간, 삭제 시간, 생성자, 수정자, 삭제자 관리

---
+++ com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/domain/model/Hub.java
@@ -0,0 +1,60 @@
+package com.first_class.msa.hub.domain.model;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "hubs")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hub {
+
+    @Id @Tsid
+    @Column(name = "hub_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "latitude", nullable = false)
+    private double latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private double longitude;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "address_detail")
+    private String addressDetail;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "modified_at" , nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @Column(name = "modified_by", nullable = false)
+    private String modifiedBy;
+
+    @Column(name = "deleted_by")
+    private String deletedBy;
+}
