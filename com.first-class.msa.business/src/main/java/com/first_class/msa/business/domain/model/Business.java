cf3a5cbaad5918d4f5a27137c668415315efb38a
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-11 16:20:16 +0900

    [ FEAR ] 업체 빌더 생성자 생성 및 업체 생성 메서드 구현 ( #12 )

--- com.first-class.msa.business/src/main/java/com/first_class/msa/business/domain/model/Business.java
+++ com.first-class.msa.business/src/main/java/com/first_class/msa/business/domain/model/Business.java
@@ -1,9 +1,11 @@
 package com.first_class.msa.business.domain.model;
 
 
+import com.first_class.msa.business.presentation.request.ReqBusinessPostDTO;
 import io.hypersistence.utils.hibernate.id.Tsid;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -38,7 +40,7 @@ public class Business {
     private String address;
 
     @Column(name = "address_detail")
-    private String address_detail;
+    private String addressDetail;
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
@@ -52,11 +54,34 @@ public class Business {
     private LocalDateTime deletedAt;
 
     @Column(name = "created_by", updatable = false)
-    private String createdBy;
+    private Long createdBy;
 
     @Column(name = "modified_by", nullable = false)
-    private String modifiedBy;
+    private Long modifiedBy;
 
     @Column(name = "deleted_by")
-    private String deletedBy;
+    private Long deletedBy;
+
+    @Builder
+    public Business(Long userId, Long hubId, String name, BusinessType type, String address, String addressDetail) {
+        this.userId = userId;
+        this.hubId = hubId;
+        this.name = name;
+        this.type = type;
+        this.address = address;
+        this.addressDetail = addressDetail;
+        this.createdBy = userId;
+        this.modifiedBy = userId;
+    }
+
+    public static Business createBusiness(Long userId, ReqBusinessPostDTO dto) {
+        return Business.builder()
+                .userId(userId)
+                .hubId(dto.getBusinessDTO().getHubId())
+                .name(dto.getBusinessDTO().getName())
+                .type(BusinessType.fromLabel(dto.getBusinessDTO().getType()))
+                .address(dto.getBusinessDTO().getAddress())
+                .addressDetail(dto.getBusinessDTO().getAddressDetail())
+                .build();
+    }
 }
