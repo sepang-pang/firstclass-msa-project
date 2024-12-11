cc38c0237742e239a288002f05ddc52257102615
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-11 18:42:03 +0900

    [ FEAT ] 업체 등록 로직 리팩토링 및 허브 검증 추가 ( #12 )
    - 업체 등록 시 중복 업체명 검증과 허브 존재 여부 검증을 하나의 메서드로 통합
    - 허브 존재 여부 검증을 위해 HubClient를 사용하여 허브 ID 검증 추가

--- com.first-class.msa.business/src/main/java/com/first_class/msa/business/application/service/BusinessService.java
+++ com.first-class.msa.business/src/main/java/com/first_class/msa/business/application/service/BusinessService.java
@@ -3,6 +3,7 @@ package com.first_class.msa.business.application.service;
 import com.first_class.msa.business.application.dto.ResBusinessPostDTO;
 import com.first_class.msa.business.domain.model.Business;
 import com.first_class.msa.business.domain.repository.BusinessRepository;
+import com.first_class.msa.business.infrastructure.client.HubClient;
 import com.first_class.msa.business.presentation.request.ReqBusinessPostDTO;
 import com.sun.jdi.request.DuplicateRequestException;
 import lombok.RequiredArgsConstructor;
@@ -14,23 +15,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class BusinessService {
 
     private final BusinessRepository businessRepository;
+    private final HubClient hubClient;
 
     @Transactional
     public ResBusinessPostDTO postBy(Long userId, ReqBusinessPostDTO dto) {
 
-        if (isDuplicateName(dto.getBusinessDTO().getName())) {
-            throw new DuplicateRequestException("이미 존재하는 업체명입니다 : " + dto.getBusinessDTO().getName());
-        }
-
-        // --
-        // TODO : 허브 검증 [ dto.getBusinessDTO().getHubId(); ]
-        // --
+        validateBusiness(dto);
 
         Business businessForSaving = Business.createBusiness(userId, dto);
 
         businessRepository.save(businessForSaving);
 
         return ResBusinessPostDTO.of(businessForSaving);
+    }
+
+    private void validateBusiness(ReqBusinessPostDTO dto) {
+        if (isDuplicateName(dto.getBusinessDTO().getName())) {
+            throw new DuplicateRequestException("이미 등록된 업체명입니다. 다른 이름을 사용하거나 기존 업체 정보를 확인해주세요");
+        }
+
+        if (!hubClient.existsBy(dto.getBusinessDTO().getHubId())) {
+            throw new IllegalArgumentException("허브 정보를 찾을 수 없습니다. 허브 ID를 확인해주세요.");
+        }
     }
 
     private boolean isDuplicateName(String name) {
