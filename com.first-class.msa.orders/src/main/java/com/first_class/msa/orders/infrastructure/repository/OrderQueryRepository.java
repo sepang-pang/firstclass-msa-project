0240b12ec235b4c9e72fdc3a62fcad716940d1aa
jshstar <jshstar97@gmail.com>
2024-12-11 12:54:51 +0900

    [ FEAT ] QueryDsl Config 추가 ( #4 )

--- com.first-class.msa.orders/src/main/java/com/first_class/msa/orders/infrastructure/repository/OrderQueryRepository.java
+++ com.first-class.msa.orders/src/main/java/com/first_class/msa/orders/infrastructure/repository/OrderQueryRepository.java
@@ -1,10 +1,5 @@
 package com.first_class.msa.orders.infrastructure.repository;
 
-import org.springframework.data.domain.Page;
-
-import com.first_class.msa.orders.domain.model.Order;
-import com.first_class.msa.orders.domain.repository.OrderRepository;
-
-public interface OrderQueryRepository extends OrderRepository {
+public interface OrderQueryRepository {
 
 }
