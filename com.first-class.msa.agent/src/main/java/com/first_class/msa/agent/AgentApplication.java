cbb9858d0e7a109fa4327d24d81909eca490edd3
jshstar <jshstar97@gmail.com>
2024-12-12 23:34:04 +0900

    [ FEAT ] Feign Client JpaAudition 추가 설정 ( #24 )

--- com.first-class.msa.agent/src/main/java/com/first_class/msa/agent/AgentApplication.java
+++ com.first-class.msa.agent/src/main/java/com/first_class/msa/agent/AgentApplication.java
@@ -2,8 +2,12 @@ package com.first_class.msa.agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@FeignClient
+@EnableJpaAuditing
 public class AgentApplication {
 
 	public static void main(String[] args) {
