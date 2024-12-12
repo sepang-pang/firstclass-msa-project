19f7501ad65f180f1b49115bd1ae2be7146b3dde
jshstar <jshstar97@gmail.com>
2024-12-12 23:33:13 +0900

    [ INIT ] GlobalException 구현 ( #24 )

---
+++ com.first-class.msa.agent/src/main/java/com/first_class/msa/agent/libs/message/ErrorMessage.java
@@ -0,0 +1,28 @@
+package com.first_class.msa.agent.libs.message;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorMessage {
+
+	/**
+	 * User
+	 */
+	INVALID_USER_ROLE(HttpStatus.FORBIDDEN, "해당 유저는 접근이 불가한 서비스입니다."),
+	INVALID_USER_ROLE_HUB_MANAGER(HttpStatus.FORBIDDEN, "허브 매니저 권한 인증에 실패했습니다."),
+	INVALID_USER_ROLE_BUSINESS_MANAGER(HttpStatus.FORBIDDEN, "업체 매니저 권한 인증에 실패했습니다."),
+	INVALID_USER_ROLE_DELIVERY_MANAGER(HttpStatus.FORBIDDEN, "배송 매니저 권한 인증에 실패했습니다."),
+
+	/**
+	 * AGENT
+	 */
+	SEQUENCE_EMPTY(HttpStatus.BAD_REQUEST, "배송 순번은 필수입니다."),
+	SEQUENCE_NOT_MINUS(HttpStatus.BAD_REQUEST, "배송 순번은 음수일 수 없습니다.");
+
+	private final HttpStatus httpStatus;
+	private final String message;
+}
