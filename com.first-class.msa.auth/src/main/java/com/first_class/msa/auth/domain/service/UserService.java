c6d431b833fc9b752c190e90b29be33f0ebfac21
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-12 11:29:38 +0900

    [ REMOVE ] 기존 권한 검증 로직 제거 ( #20 )

--- com.first-class.msa.auth/src/main/java/com/first_class/msa/auth/domain/service/UserService.java
+++ com.first-class.msa.auth/src/main/java/com/first_class/msa/auth/domain/service/UserService.java
@@ -2,7 +2,6 @@ package com.first_class.msa.auth.domain.service;
 
 import com.first_class.msa.auth.config.jwt.JwtUtil;
 import com.first_class.msa.auth.domain.dto.ReqLoginDTO;
-import com.first_class.msa.auth.domain.dto.ReqRoleValidationDTO;
 import com.first_class.msa.auth.domain.model.User;
 import com.first_class.msa.auth.domain.dto.ReqUserPostDTO;
 import com.first_class.msa.auth.domain.repository.UserRepository;
@@ -48,16 +47,6 @@ public class UserService {
 
         // JWT 토큰 생성
         return jwtUtil.generateToken(user.getUserId(), user.getAccount(), user.getRole());
-    }
-
-
-    public boolean checkBy(Long userId, ReqRoleValidationDTO dto) {
-
-        User userForRoleChecking = findById(userId);
-
-        return dto.getRoles() == null
-                ? userForRoleChecking.getRole().name().equals(dto.getRole())
-                : dto.getRoles().contains(userForRoleChecking.getRole().name());
     }
 
 
