f047210302f2108fe6bbd603e0f6fa9c0b64d8a9
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-13 17:50:32 +0900

    [ RENAME ] 회원 권한 MANAGER -> MASTER 이름 수정 ( #27 )

--- com.first-class.msa.auth/src/main/java/com/first_class/msa/auth/domain/model/Role.java
+++ com.first-class.msa.auth/src/main/java/com/first_class/msa/auth/domain/model/Role.java
@@ -1,5 +1,5 @@
 package com.first_class.msa.auth.domain.model;
 
 public enum Role {
-    MANAGER,HUB_MANAGER,DELIVERY_MANAGER,BUSINESS_MANAGER
+    MASTER,HUB_MANAGER,DELIVERY_MANAGER,BUSINESS_MANAGER
 }
