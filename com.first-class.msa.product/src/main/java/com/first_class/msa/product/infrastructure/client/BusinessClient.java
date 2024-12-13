b0ee26354f389e640a725f8cd1fefb93c6b2f4fb
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-13 22:39:12 +0900

    [ REMOVE ] BusinessClient @RequestMapping("/external") 제거 ( #26 )

--- com.first-class.msa.product/src/main/java/com/first_class/msa/product/infrastructure/client/BusinessClient.java
+++ com.first-class.msa.product/src/main/java/com/first_class/msa/product/infrastructure/client/BusinessClient.java
@@ -5,16 +5,14 @@ import com.first_class.msa.product.application.service.BusinessService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "business-service")
-@RequestMapping("/external")
 public interface BusinessClient extends BusinessService {
 
-    @GetMapping("/business/{businessId}")
+    @GetMapping("/external/business/{businessId}")
     ExternalResBusinessGetByIdDTO getBy(@PathVariable("businessId") Long businessId);
 
-    @GetMapping("/business/{businessId}/exists")
+    @GetMapping("/external/business/{businessId}/exists")
     boolean existsBy(@PathVariable("businessId") Long businessId);
 
 }
