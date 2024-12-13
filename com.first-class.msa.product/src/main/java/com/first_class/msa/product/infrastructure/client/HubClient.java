108475c883d98b05efea17eb586aa59066f1fb13
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-13 18:59:41 +0900

    [ FEAT ] HubClient 메서드 엔드포인트 수정 ( #26 )

--- com.first-class.msa.product/src/main/java/com/first_class/msa/product/infrastructure/client/HubClient.java
+++ com.first-class.msa.product/src/main/java/com/first_class/msa/product/infrastructure/client/HubClient.java
@@ -1,15 +1,16 @@
 package com.first_class.msa.product.infrastructure.client;
 
+import com.first_class.msa.product.application.service.HubService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "hub-service")
-public interface HubClient {
+public interface HubClient extends HubService {
 
-    @GetMapping("/hubs/{hubId}/exists")
+    @GetMapping("/external/hubs/{hubId}/exists")
     boolean existsBy(@PathVariable Long hubId);
 
-    @GetMapping("/hub/by-user/{userId}")
+    @GetMapping("/external/hubs/user/{userId}/id")
     Long getHubIdBy(@PathVariable Long userId);
 }
