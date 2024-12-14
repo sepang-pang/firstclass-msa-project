b0ff321ae384da053483f38618ffe51450b80faa
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-15 03:43:04 +0900

    [ FEAT ] redis 캐싱 설정 파일 생성 ( #26 )

---
+++ com.first-class.msa.product/src/main/java/com/first_class/msa/product/infrastructure/configuration/CacheConfig.java
@@ -0,0 +1,37 @@
+package com.first_class.msa.product.infrastructure.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+
+        RedisCacheConfiguration configuration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofSeconds(60))
+                .computePrefixWith(CacheKeyPrefix.simple())
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json())
+                );
+
+        return RedisCacheManager
+                .builder(connectionFactory)
+                .cacheDefaults(configuration)
+                .build();
+    }
+}
+
