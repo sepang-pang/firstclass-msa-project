0c5963d2d7dda8607315e593d2d21d748be22bdf
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-15 22:09:45 +0900

    [ FEAT ] 지리적 좌표를 이용한 거리 계산 메서드 추가 ( #36 )

--- com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/infrastructure/repository/HubTransitInfo/JpaHubTransitInfoRepository.java
+++ com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/infrastructure/repository/HubTransitInfo/JpaHubTransitInfoRepository.java
@@ -2,6 +2,17 @@ package com.first_class.msa.hub.infrastructure.repository.HubTransitInfo;
 
 import com.first_class.msa.hub.domain.model.HubTransitInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JpaHubTransitInfoRepository extends JpaRepository<HubTransitInfo, Long> {
+
+    @Query(value = "select ST_DistanceSphere(" +
+            "ST_MakePoint(:departureLongitude, :departureLatitude), " +
+            "ST_MakePoint(:arrivalLongitude, :arrivalLatitude))", nativeQuery = true)
+    double calculateDistanceByCoordinates(@Param("departureLatitude") double departureLatitude,
+                                          @Param("departureLongitude") double departureLongitude,
+                                          @Param("arrivalLatitude") double arrivalLatitude,
+                                          @Param("arrivalLongitude") double arrivalLongitude);
+
 }
