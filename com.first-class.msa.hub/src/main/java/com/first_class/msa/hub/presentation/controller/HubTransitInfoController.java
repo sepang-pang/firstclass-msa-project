49fdebaf58d4428ac4d8b82803a02e832b1c26ec
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-16 02:45:55 +0900

    [ FEAT ] 허브 이동 정보 수정 구현 ( #40 )

--- com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/presentation/controller/HubTransitInfoController.java
+++ com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/presentation/controller/HubTransitInfoController.java
@@ -5,10 +5,14 @@ import com.first_class.msa.hub.application.dto.transit.ResHubTransitInfoGetDTO;
 import com.first_class.msa.hub.application.dto.transit.ResHubTransitInfoPostDTO;
 import com.first_class.msa.hub.application.service.HubTransitInfoService;
 import com.first_class.msa.hub.presentation.request.transit.ReqHubTransitInfoPostDTO;
+import com.first_class.msa.hub.presentation.request.transit.ReqHubTransitInfoPutByIdDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +24,7 @@ public class HubTransitInfoController {
     @PostMapping("/hub-transit-infos")
     public ResponseEntity<ResDTO<ResHubTransitInfoPostDTO>> postBy(@RequestHeader("X-User-Id") Long userId,
                                                                    @RequestHeader("X-User-Account") String account,
-                                                                   @RequestBody ReqHubTransitInfoPostDTO dto) {
+                                                                   @Valid @RequestBody ReqHubTransitInfoPostDTO dto) {
 
         return new ResponseEntity<>(
                 ResDTO.<ResHubTransitInfoPostDTO>builder()
@@ -41,6 +45,24 @@ public class HubTransitInfoController {
                         .code(HttpStatus.OK.value())
                         .message("허브 이동 정보 조회에 성공하였습니다.")
                         .data(hubTransitInfoService.getBy(departureHubId, arrivalHubId))
+                        .build(),
+                HttpStatus.OK
+        );
+    }
+
+    @PutMapping("/hub-transit-infos/{hubTransitInfoId}")
+    public ResponseEntity<ResDTO<Objects>> putBy(@RequestHeader("X-User-Id") Long userId,
+                                                 @RequestHeader("X-User-Account") String account,
+                                                 @Valid @RequestBody ReqHubTransitInfoPutByIdDTO dto,
+                                                 @PathVariable(name = "hubTransitInfoId") Long hubTransitInfoId) {
+
+
+        hubTransitInfoService.putBy(userId, account, hubTransitInfoId, dto);
+
+        return new ResponseEntity<>(
+                ResDTO.<Objects>builder()
+                        .code(HttpStatus.OK.value())
+                        .message("허브 이동 정보 수정에 성공하였습니다.")
                         .build(),
                 HttpStatus.OK
         );
