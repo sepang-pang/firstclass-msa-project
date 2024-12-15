8dab4e3f6d901c1fd35f295a0084961919baa025
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-15 22:09:45 +0900

    [ FEAT ] 지리적 좌표를 이용한 거리 계산 메서드 추가 ( #36 )

--- com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/domain/repository/HubTransitInfoRepository.java
+++ com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/domain/repository/HubTransitInfoRepository.java
@@ -1,4 +1,10 @@
 package com.first_class.msa.hub.domain.repository;
 
+import com.first_class.msa.hub.domain.model.HubTransitInfo;
+
 public interface HubTransitInfoRepository {
+
+    double calculateDistanceByCoordinates(double departureLat, double departureLon, double arrivalLat, double arrivalLon);
+
+    void save(HubTransitInfo transitInfo);
 }
--- com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/infrastructure/repository/HubTransitInfo/HubTransitInfoRepositoryImpl.java
+++ com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/infrastructure/repository/HubTransitInfo/HubTransitInfoRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.first_class.msa.hub.infrastructure.repository.HubTransitInfo;
 
+import com.first_class.msa.hub.domain.model.HubTransitInfo;
 import com.first_class.msa.hub.domain.repository.HubTransitInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,16 @@ import org.springframework.stereotype.Repository;
 public class HubTransitInfoRepositoryImpl implements HubTransitInfoRepository {
 
     private final JpaHubTransitInfoRepository jpaHubTransitInfoRepository;
+
+
+    @Override
+    public double calculateDistanceByCoordinates(double departureLat, double departureLon, double arrivalLat, double arrivalLon) {
+        return jpaHubTransitInfoRepository.calculateDistanceByCoordinates(departureLat, departureLon, arrivalLat, arrivalLon);
+    }
+
+    @Override
+    public void save(HubTransitInfo hubTransitInfo) {
+        jpaHubTransitInfoRepository.save(hubTransitInfo);
+    }
+
 }
