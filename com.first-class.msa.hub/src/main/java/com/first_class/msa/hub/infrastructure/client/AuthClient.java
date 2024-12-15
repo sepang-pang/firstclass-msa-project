25c416431401efdf42876f7f28c953f70fb5eb92
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-15 10:51:16 +0900

    [ FEAT ] AuthClient 구조 변경 및 AuthService 인터페이스 분리 ( #32 )

--- com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/infrastructure/client/AuthClient.java
+++ com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/infrastructure/client/AuthClient.java
@@ -1,14 +1,16 @@
 package com.first_class.msa.hub.infrastructure.client;
 
-import com.first_class.msa.hub.application.dto.ReqRoleValidationDTO;
+import com.first_class.msa.hub.application.dto.external.ResRoleGetByUserIdDTO;
+import com.first_class.msa.hub.application.service.AuthService;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "auth-service")
-public interface AuthClient {
+public interface AuthClient extends AuthService {
 
-    @PostMapping("/auth/{userId}")
-    boolean checkBy(@PathVariable(name = "userId") Long userId, @RequestBody ReqRoleValidationDTO dto);
+    @GetMapping("/external/users/{userId}")
+    ResRoleGetByUserIdDTO getRoleBy(@PathVariable(name = "userId") Long userId);
 }
