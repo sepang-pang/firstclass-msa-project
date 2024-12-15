afb929f55416c26d9eeed1a587d2ce89196e1649
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-15 12:38:57 +0900

    [ FEAT ] 허브 간 이동정보 엔티티 설계 ( #36 )

---
+++ com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/domain/model/HubTransitInfo.java
@@ -0,0 +1,56 @@
+package com.first_class.msa.hub.domain.model;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "p_hub_transit_infos")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HubTransitInfo {
+
+    @Id @Tsid
+    @Column(name = "hub_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "arrival_hub_id", referencedColumnName = "hub_id")
+    private Hub arrivalHub;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "departure_hub_id", referencedColumnName = "hub_id")
+    private Hub departureHub;
+
+    @Column(name = "transit_time")
+    private LocalDateTime transitTime;
+
+    @Column(name = "distance")
+    private double distance;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @Column(name = "modified_by", nullable = false)
+    private String modifiedBy;
+
+    @Column(name = "deleted_by")
+    private String deletedBy;
+}
