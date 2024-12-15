0f6a9b8df672f0d3c76da91cd19ca1746754a37c
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-16 02:45:41 +0900

    [ FEAT ] 허브 이동 정보 단일 조회 쿼리 메서드 생성 ( #40 )

--- com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/infrastructure/repository/HubTransitInfo/JpaHubTransitInfoRepository.java
+++ com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/infrastructure/repository/HubTransitInfo/JpaHubTransitInfoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JpaHubTransitInfoRepository extends JpaRepository<HubTransitInfo, Long> {
 
@@ -19,4 +20,6 @@ public interface JpaHubTransitInfoRepository extends JpaRepository<HubTransitInf
 
 
     List<HubTransitInfo> findAllByDeletedAtIsNull();
+
+    Optional<HubTransitInfo> findByIdAndDeletedAtIsNull(Long id);
 }
