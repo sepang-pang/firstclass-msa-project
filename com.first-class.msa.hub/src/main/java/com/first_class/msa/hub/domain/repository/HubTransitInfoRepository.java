1ad62305f8aef8f95f5cfc7e92c7bb9e58a5e356
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-16 02:45:41 +0900

    [ FEAT ] 허브 이동 정보 단일 조회 쿼리 메서드 생성 ( #40 )

--- com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/domain/repository/HubTransitInfoRepository.java
+++ com.first-class.msa.hub/src/main/java/com/first_class/msa/hub/domain/repository/HubTransitInfoRepository.java
@@ -3,6 +3,7 @@ package com.first_class.msa.hub.domain.repository;
 import com.first_class.msa.hub.domain.model.HubTransitInfo;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HubTransitInfoRepository {
 
@@ -11,4 +12,6 @@ public interface HubTransitInfoRepository {
     List<HubTransitInfo> findAllByDeletedAtIsNull();
 
     void save(HubTransitInfo transitInfo);
+
+    Optional<HubTransitInfo> findByIdAndDeletedAtIsNull(Long hubTransitInfoId);
 }
