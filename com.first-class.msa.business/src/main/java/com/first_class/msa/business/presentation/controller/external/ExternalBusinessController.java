e08fdce3aea4ab67ed44a284a78a20c024e7b992
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-16 21:30:16 +0900

    [ FEAT ] 매니저 ID 기반 업체 조회 Feign Client API 개발 ( #48 )

--- com.first-class.msa.business/src/main/java/com/first_class/msa/business/presentation/controller/external/ExternalBusinessController.java
+++ com.first-class.msa.business/src/main/java/com/first_class/msa/business/presentation/controller/external/ExternalBusinessController.java
@@ -1,10 +1,12 @@
 package com.first_class.msa.business.presentation.controller.external;
 
 import com.first_class.msa.business.application.dto.external.ExternalResBusinessGetByIdDTO;
+import com.first_class.msa.business.application.dto.external.ExternalResBusinessGetByUserIdDTO;
 import com.first_class.msa.business.application.service.BusinessService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +23,10 @@ public class ExternalBusinessController {
     @GetMapping("/external/business/{businessId}")
     public ExternalResBusinessGetByIdDTO getBy(@PathVariable("businessId") Long businessId) {
         return businessService.getBy(businessId);
+    }
+
+    @GetMapping("/external/business")
+    public ExternalResBusinessGetByUserIdDTO getByUserId(@RequestParam Long userId) {
+        return businessService.getByUserId(userId);
     }
 }
