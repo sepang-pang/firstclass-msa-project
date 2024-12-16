20ed71eade1fdab86d40cc3e247b80cc05a7936f
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-16 21:31:29 +0900

    [ REFACTOR ] 중복 API "/external" @RequestMapping 으로 관리 ( #48 )

--- com.first-class.msa.business/src/main/java/com/first_class/msa/business/presentation/controller/external/ExternalBusinessController.java
+++ com.first-class.msa.business/src/main/java/com/first_class/msa/business/presentation/controller/external/ExternalBusinessController.java
@@ -4,28 +4,26 @@ import com.first_class.msa.business.application.dto.external.ExternalResBusiness
 import com.first_class.msa.business.application.dto.external.ExternalResBusinessGetByUserIdDTO;
 import com.first_class.msa.business.application.service.BusinessService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/external")
 public class ExternalBusinessController {
 
     private final BusinessService businessService;
 
-    @GetMapping("/external/business/{businessId}/exists")
+    @GetMapping("/business/{businessId}/exists")
     public boolean existsBy(@PathVariable("businessId") Long businessId) {
         return businessService.existsBy(businessId);
     }
 
-    @GetMapping("/external/business/{businessId}")
+    @GetMapping("/business/{businessId}")
     public ExternalResBusinessGetByIdDTO getBy(@PathVariable("businessId") Long businessId) {
         return businessService.getBy(businessId);
     }
 
-    @GetMapping("/external/business")
+    @GetMapping("/business")
     public ExternalResBusinessGetByUserIdDTO getByUserId(@RequestParam Long userId) {
         return businessService.getByUserId(userId);
     }
