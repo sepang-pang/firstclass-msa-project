d9ab29d9e4b5cf26469f59b05b14aebd9941b25c
jshstar <jshstar97@gmail.com>
2024-12-16 22:46:10 +0900

    [ RENAME ] HubClient 메서드명 변경  ( #47 )

--- com.first-class.msa.orders/src/main/java/com/first_class/msa/orders/infrastructure/client/HubClient.java
+++ com.first-class.msa.orders/src/main/java/com/first_class/msa/orders/infrastructure/client/HubClient.java
@@ -4,13 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.first_class.msa.orders.application.dto.ResHubDTO;
 import com.first_class.msa.orders.application.service.HubService;
 
 @FeignClient(name = "hub-service")
 public interface HubClient extends HubService {
 
-	@GetMapping("/hubs/{userId}")
-	ResHubDTO getHubBy(@PathVariable(name = "userId") Long userId);
+	@GetMapping("/external/hubs/user/{userId}/id")
+	Long getHubByID(@PathVariable(name = "userId") Long userId);
 
 }
