9a3feb3f91a009e82b8d1ed4e8a97def12a928a6
jshstar <jshstar97@gmail.com>
2024-12-16 22:44:24 +0900

    [ RENAME ] Feign Client 변경으로 인한 DeliveryService 메서드명 변경 및 불필요한 DTO 삭제  ( #47 )

--- com.first-class.msa.orders/src/main/java/com/first_class/msa/orders/application/dto/ResDeliveryOrderSearchDetailDTO.java
+++
@@ -1,9 +0,0 @@
-package com.first_class.msa.orders.application.dto;
-
-import lombok.Getter;
-
-@Getter
-public class ResDeliveryOrderSearchDetailDTO {
-	private Long deliveryUserId;
-	private Long orderId;
-}
