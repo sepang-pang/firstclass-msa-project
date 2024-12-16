4dc4e39bc09c012818fea1a61429ebfa650b7ca7
jshstar <jshstar97@gmail.com>
2024-12-16 22:47:30 +0900

    [ RENAME ] BusinessClient Url 및 파라미터 변경 ( #47 )

--- com.first-class.msa.orders/src/main/java/com/first_class/msa/orders/infrastructure/client/BusinessClient.java
+++ com.first-class.msa.orders/src/main/java/com/first_class/msa/orders/infrastructure/client/BusinessClient.java
@@ -3,15 +3,16 @@ package com.first_class.msa.orders.infrastructure.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.first_class.msa.orders.application.dto.ResBusinessDTO;
 import com.first_class.msa.orders.application.service.BusinessService;
 @FeignClient(name = "business-service")
 public interface BusinessClient extends BusinessService {
 
-	@GetMapping({"/business/{businessId}"})
+	@GetMapping({"/external/business/{businessId}"})
 	ResBusinessDTO getBusinessBy(@PathVariable(name = "businessId") Long businessId);
 
-	@GetMapping({"/business/{userId}"})
-	ResBusinessDTO getBusinessUserBy(@PathVariable Long userId);
+	@GetMapping({"/external/business"})
+	ResBusinessDTO getBusinessUserBy(@RequestParam Long userId);
 }
